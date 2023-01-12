510cb94d00372d677f1135ebe0d607a93984d465
AECRobotics <93950409+AECRobotics@users.noreply.github.com>
2023-01-12 11:36:22 -0800

    added hsv based color matching

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionUtils/ConeStateFinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionUtils/ConeStateFinder.java
@@ -31,6 +31,9 @@ public class ConeStateFinder {
     private static double colorDistSimilarityThreshold = 23.0*23.0;
     private static double colorDotProdSimilaryThreshold = 0.99;
     private static double colorMagnitudeSimilarityThreshold = 15.0*15.0;
+    private static double colorHueSimilarityThreshold = 21.0;
+    private static double colorSatSimilarityThreshold = 26.0;
+    private static double colorValSimilarityThreshold = 36.0;
 
     public static String debugOutput = "";
 
@@ -83,6 +86,41 @@ public class ConeStateFinder {
         return Math.sqrt(a*a+r*r+g*g+b*b);
     }
 
+    public static double[] getHSV(int c) {
+        double r = (c&0x00ff0000)>>16;
+        double g = (c&0x0000ff00)>>8;
+        double b = (c&0x000000ff);
+        r/=255;
+        g/=255;
+        b/=255;
+        double maxc = Math.max(r, Math.max(g, b));
+        double minc = Math.min(r, Math.min(g, b));
+        double v = maxc;
+        if (minc == maxc) {
+            //output.add(0,.0,0.0,v);
+            return new double[]{0.0, 0.0, v};
+        }
+        double s = (maxc-minc) / maxc;
+        double rc = (maxc-r) / (maxc-minc);
+        double gc = (maxc-g) / (maxc-minc);
+        double bc = (maxc-b) / (maxc-minc);
+        double h;
+        if (r == maxc) {
+            h = 0.0+bc-gc;
+        }
+        else if (g == maxc) {
+            h = 2.0+rc-bc;
+        }
+        else {
+            h = 4.0+gc-rc;
+        }
+        h = (h/6.0) % 1.0;
+        h*=360;
+        s*=100;
+        v*=100;
+        return new double[]{h, s, v};
+    }
+
     public static boolean matchesColor(int c1, int c2) {
         boolean sqrtDistMatches = sqrdColorDistance(c1, c2) <= colorDistSimilarityThreshold;
         //debugOutput+=("dist: " + sqrdColorDistance(c1, c2) + ", ");
@@ -99,6 +137,19 @@ public class ConeStateFinder {
         if(dotProductMatches) {
             if(Math.abs(getColorMagnitude(c1) - getColorMagnitude(c2)) < colorMagnitudeSimilarityThreshold) {
                 return true;
+            }
+        }
+        double[] hc1 = getHSV(c1);
+        double[] hc2 = getHSV(c2);
+        boolean hueMatches = Math.abs(hc1[0]-hc2[0]) <= colorHueSimilarityThreshold;
+        boolean satMatches = Math.abs(hc1[1]-hc2[1]) <= colorSatSimilarityThreshold;
+        boolean valMatches = Math.abs(hc1[2]-hc2[2]) <= colorValSimilarityThreshold;
+        //console.log(h1 + ", " + h2);
+        if(hueMatches) {
+            if(satMatches) {
+                if(valMatches) {
+                    return true;
+                }
             }
         }
         return false;
